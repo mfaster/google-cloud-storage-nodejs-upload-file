312d77e8717370bc7ecda7fc252643ff0837db46
Q_Q <mfaster@mfaster.com>
2021-12-15 01:54:13 +0700

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,16 @@ const initRoutes = require("./src/routes");
 app.use(express.urlencoded({ extended: true }));
 initRoutes(app);
 
-const port = 8080;
+// const port = 8080;
+// app.listen(port, () => {
+//   console.log(`Running at localhost:${port}`);
+// });
+
+
+// # use alternate localhost and the port Heroku assigns to $PORT
+const host = '0.0.0.0';
+const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
-  console.log(`Running at localhost:${port}`);
+  console.log("Server started.......");
 });
