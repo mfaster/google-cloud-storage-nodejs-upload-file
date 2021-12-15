9e140570b0057701a74e604cf66d29ec012c17cf
Q_Q <mfaster@mfaster.com>
2021-12-15 23:23:26 +0700

    Update server.js

--- server.js
+++ server.js
@@ -7,8 +7,17 @@ const app = express();
 // };
 
 // app.use(cors(corsOptions));
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*'); //หรือใส่แค่เฉพาะ domain ที่ต้องการได้
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 
 const initRoutes = require("./src/routes");
+
+
 
 app.use(express.urlencoded({ extended: true }));
 initRoutes(app);
