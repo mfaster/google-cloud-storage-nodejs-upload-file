754991e4adadef82cda27f88b88762e4a0a45238
Paskon <paskon.i@transcodeglobal.com>
2021-12-16 05:13:52 +0700

    update cors

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const app = express();
 
 
 app.use(cors())
+app.options("*", cors())
 app.use(function (req, res, next) {
   // res.setHeader('Access-Control-Allow-Origin', '*'); //หรือใส่แค่เฉพาะ domain ที่ต้องการได้
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
@@ -49,7 +50,7 @@ app.use(function (req, res, next) {
 const initRoutes = require("./src/routes");
 
 
-// app.use(express.json())
+app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 initRoutes(app);
 
