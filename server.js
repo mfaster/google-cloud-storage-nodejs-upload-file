fa3851cf5882b96ce5d9b29495f8515f4a202c7d
Q_Q <mfaster@mfaster.com>
2021-12-16 05:17:28 +0700

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.options("*", cors())
 app.use(function (req, res, next) {
   // res.setHeader('Access-Control-Allow-Origin', '*'); //หรือใส่แค่เฉพาะ domain ที่ต้องการได้
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, multipart/form-data'); //เพิ่ม multipart
+  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers'); //เพิ่ม multipart
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
