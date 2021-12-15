ddcebc7ca50ae0db493195b99a37c83ae3438cd0
Q_Q <mfaster@mfaster.com>
2021-12-16 01:21:43 +0700

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const app = express();
 //   origin: "gcloud.adonwebthailand.com",
 // };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*'); //หรือใส่แค่เฉพาะ domain ที่ต้องการได้
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
